e4595811b7a5dbe28b78e0c03401f77c1212e904
unknown <sairakesh.cse@citchennai.net>
2024-03-08 13:58:29 +0530

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,28 @@
 
 import React from 'react';
 import './Navbar.css';
+import resumePDF from '../assets/Sai_Rakesh_K (2).pdf';
 
 const Navbar = ({ scrollToProjects }) => {
   const handleContactClick = () => {
-    
     window.scrollTo({
       top: document.body.scrollHeight,
       behavior: 'smooth'
     });
   };
 
+  const handleResumeClick = () => {
+    
+      window.open(resumePDF, '_blank');
+    };
+
   return (
     <div className="nav">
       <div className="name" onClick={scrollToProjects}>Sai Rakesh K</div>
       <ul>
         <li onClick={scrollToProjects}>Projects</li>
-        <li onClick={handleContactClick}>Contact</li> 
+        <li onClick={handleContactClick}>Contact</li>
+        <li onClick={handleResumeClick}>Resume</li> 
       </ul>
     </div>
   );
